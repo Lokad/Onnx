720900b4bb43ecf2967a69f57ed912d99ac31cdf
Joannes Vermorel <joannes.vermorel@lokad.com>
2026-01-02 14:56:50 +0100

    Add focused MatMul2D regression tests

--- tests/Lokad.Onnx.Tensors.Tests/TestHelpers.cs
+++ tests/Lokad.Onnx.Tensors.Tests/TestHelpers.cs
@@ -4,11 +4,15 @@ namespace Lokad.Onnx.Tensors.Tests;
 
 internal sealed class HardwareConfigScope : IDisposable
 {
+    private static readonly object sync = new();
+    private readonly bool lockHeld;
     private readonly bool useSimd;
     private readonly bool useIntrinsics;
 
     public HardwareConfigScope(bool useSimd, bool useIntrinsics)
     {
+        System.Threading.Monitor.Enter(sync);
+        lockHeld = true;
         this.useSimd = HardwareConfig.UseSimd;
         this.useIntrinsics = HardwareConfig.UseIntrinsics;
         HardwareConfig.UseSimd = useSimd;
@@ -19,5 +23,9 @@ internal sealed class HardwareConfigScope : IDisposable
     {
         HardwareConfig.UseSimd = useSimd;
         HardwareConfig.UseIntrinsics = useIntrinsics;
+        if (lockHeld)
+        {
+            System.Threading.Monitor.Exit(sync);
+        }
     }
 }
