344d8411d44a057c57983971eb416740db521310
Joannes Vermorel <joannes.vermorel@lokad.com>
2026-09-11 02:07:37 +0200

    fix(cli): propagate CLI exit codes through lonnx.cmd wrapper

--- lonnx.cmd
+++ lonnx.cmd
@@ -7,12 +7,12 @@ REM From Alec Mev https://superuser.com/questions/35698/how-to-supress-terminate
 IF [%JUSTTERMINATE%] == [OKAY] (
     SET JUSTTERMINATE=
     src\Lokad.Onnx.CLI\bin\Release\net10.0\Lokad.Onnx.CLI.exe %*
+    CALL SET ERROR_CODE=%%ERRORLEVEL%%
 ) ELSE (
     SET JUSTTERMINATE=OKAY
     CALL %0 %* <NUL
+    CALL SET ERROR_CODE=%%ERRORLEVEL%%
 )
 
 :end
-@endlocal
-popd
-exit /B %ERROR_CODE%
+@endlocal & popd & exit /B %ERROR_CODE%
